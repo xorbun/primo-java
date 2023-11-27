fe3f78a40de4a49c75be1a7f516a682685969bdd
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-11-27 15:45:00 +0100

    commit
    some bug fixed

--- src/Main1.java
+++ src/Main1.java
@@ -1,23 +1,44 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main1
 {
     public static void main(String[] args)
+    {
+        moltiplicazione();
+        concatenazione();
+        String[] array1={"ciao ", "a ", "tutti ", "e ", "benvenuti "};
+        String nuovaparola1="boh ";
+        String[] prova =inserisciinarray(array1,nuovaparola1);
+        for(int i=0;i<prova.length;i++)
+        {
+            System.out.printf(prova[i]);
+        }
+
+
+    }
+    public static void moltiplicazione()
     {
         int num1=3;
         int num2=5;
         int prodotto=num1*num2;
         System.out.printf("il prodotto è "+ prodotto);
-
+    }
+    public static void concatenazione()
+    {
         String parola1="ciao ";
         String parola2="a tutti ";
         System.out.printf(parola1 + parola2);
+    }
+    public static String[] inserisciinarray(String[] primoarray, String nuovaparola)
+    {
 
-        String parole []= {"ciao ","a ", "tutti ", "e ", "benvenuti"};
-        for(int i=0;i<parole.length;i++)
-        {
-            System.out.printf(parole[i]);
-        }
+        String[] secondoarray=new String[6];
+        secondoarray[0] = primoarray[0];
+        secondoarray[1] = primoarray[1];
+        secondoarray[2] = nuovaparola;
+        secondoarray[3] = primoarray[2];
+        secondoarray[4] = primoarray[3];
+        secondoarray[5] = primoarray[4];
 
+        return secondoarray;
     }
 }
--- src/main3.java
+++ src/main3.java
@@ -4,10 +4,19 @@ public class main3
 {
     public static void main(String[] args)
     {
+        perimetro();
+        pariodispari();
+        area();
+    }
+    public static void perimetro()
+    {
         int base=4;
         int altezza=5;
         int perimetro=((base*2)+(altezza*2));
         System.out.printf("il perimetro del rettangolo è "+ perimetro);
+    }
+    public static void pariodispari()
+    {
         Scanner input= new Scanner(System.in);
         System.out.printf("scrivi un numero");
         int numero= input.nextInt();
@@ -20,10 +29,14 @@ public class main3
         {
             System.out.printf("numero dispari");
         }
-        int lato1=121;
+    }
+    public static void area()
+    {
+        int lato1=3;
         int lato2=5;
         int lato3=7;
-        double perimetro2=Math.sqrt(lato1);
-        System.out.printf("il perimetro del triangolo è" + perimetro2);
+        int perimetro2=lato1+lato2+lato3;
+        double area=Math.sqrt((perimetro2*(perimetro2-lato1)*(perimetro2-lato2)*(perimetro2*lato3)));
+        System.out.printf("l'area del triangolo è" + area);
     }
 }
